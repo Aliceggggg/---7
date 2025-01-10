9279bb30c50553be79ab390dd337ecf00ec87c94
Aliceggggg <143636043+Aliceggggg@users.noreply.github.com>
2025-01-10 19:27:50 +0500

    Create math_operations.h
    Заголовочный файл с кодом для решения задачи ИКМ вариант 7

---
+++ math_operations.h
@@ -0,0 +1,23 @@
+#ifndef UNTITLED8_MATH_OPERATIONS_H
+#define UNTITLED8_MATH_OPERATIONS_H
+
+#include <string>
+using namespace std;
+
+// Функция для умножения двух чисел, представленных строками
+string multistr(const string& num1, const string& num2);
+
+// Функция для вычисления факториала числа, представленного строкой
+string factorial(const string& num_str);
+
+// Функция для определения верности ввода
+bool truenumber(const string& str);
+
+// Функция для реверсирования строки
+void reverse(string& str);
+
+// Функция для сложения двух чисел, представленных строками
+string sumstr(const string& num1, const string& num2);
+
+
+#endif
